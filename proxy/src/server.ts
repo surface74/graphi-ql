c5b384700e6e0667a38c894b9ee1c91ad35e3b9a
surface74 <surface74@example.com>
2023-12-26 00:06:34 +0300

    feat: add CORS proxy-server with base functionality

---
+++ proxy/src/server.ts
@@ -0,0 +1,75 @@
+import express from 'express';
+import { Request } from 'express';
+import cors from 'cors';
+import request from 'request';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface IRequestData {
+  endpoint: string;
+  query: string;
+}
+
+const app = express();
+const port = process.env.PORT ?? 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.options('*', cors());
+
+app.get('/', (req, res) => {
+  console.log('[GET] root');
+  res.send({ message: 'Welcome to CORS proxy-server' });
+});
+
+app.get(
+  '/proxy',
+  (req: Request<unknown, unknown, unknown, IRequestData>, res) => {
+    console.log('[GET] /proxy');
+
+    const params = req.query;
+
+    const endpoint = decodeURIComponent(params.endpoint);
+    const query = decodeURIComponent(params.query);
+
+    request
+      .post(endpoint, {
+        body: JSON.stringify({ query }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .on('error', function (error) {
+        res.statusCode = 400;
+        res.send({
+          errors: [{ message: error.message, stack: error.stack ?? '' }],
+        });
+      })
+      .pipe(res);
+  }
+);
+
+app.post('/proxy', (req: Request<unknown, unknown, IRequestData>, res) => {
+  console.log('[POST] /proxy');
+
+  const { endpoint, query } = req.body;
+
+  request
+    .post(endpoint, {
+      body: JSON.stringify({ query }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .on('error', function (err) {
+      res.statusCode = 400;
+      res.send({ errors: [{ message: err.message, stack: err.stack ?? '' }] });
+    })
+    .pipe(res);
+});
+
+app.listen(port, () =>
+  console.log(`CORS proxy-server is listening on port ${port}...`)
+);
